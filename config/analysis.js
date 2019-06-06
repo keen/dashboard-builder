659513e792c1dde39383d73bc29dc424dc7f8633
dariuszlacheta <dariusz.lacheta@keen.io>
2019-06-06 11:14:27 +0200

    refactor and changes

--- config/analysis.js
+++ config/analysis.js
@@ -3,7 +3,7 @@ import KeenAnalysis from "keen-analysis";
 const client = new KeenAnalysis({
   projectId: "5c87b64ec9e77c0001cf5b6e",
   readKey: "FB952962910C97DE3E1C6A25EB2FC6B22FDB1ACA9D572948EA18227287BC4E12",
-  masterKey: "585B781C4281234B90282FEDDF7440FA42A06455AEEAE4283F09DD95A278155F",
+  masterKey: "",
   writeKey: ""
 });
 
